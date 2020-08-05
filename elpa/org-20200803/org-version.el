7e72de346cc68d5485bfc042fb0f1217323d55d5
David <das@acm.org>
2020-08-05 11:48:06 -0400

    Updated org-mode and removed bind-key

--- elpa/org-20200803/org-version.el
+++ elpa/org-20200803/org-version.el
@@ -11,7 +11,7 @@ Inserted by installing Org mode or when a release is made."
 (defun org-git-version ()
   "The Git version of Org mode.
 Inserted by installing Org or when a release is made."
-   (let ((org-git-version "9.3.7-15-gc9abb4-elpa"))
+   (let ((org-git-version "9.3.7-16-g521d7f-elpa"))
      org-git-version))
 
 (provide 'org-version)
