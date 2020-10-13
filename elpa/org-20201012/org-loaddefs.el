f9bcb03024d15ca1f31a4684260f0ebf3438e2a3
David <das@acm.org>
2020-10-13 14:59:40 -0400

    Updated org to 20201012

--- elpa/org-20201012/org-loaddefs.el
+++ elpa/org-20201012/org-loaddefs.el
@@ -998,7 +998,7 @@ Also refresh fontification if needed.
 
 ;;;***
 
-;;;### (autoloads nil "org" "org.el" "aa928e6921e36d5e7ff54fbda8f399c8")
+;;;### (autoloads nil "org" "org.el" "f4d334424d5f9e3103f4453234182cfb")
 ;;; Generated autoloads from org.el
 
 (autoload 'org-babel-do-load-languages "org" "\
@@ -2409,15 +2409,13 @@ if ARG is `toggle'; disable the mode otherwise.
 
 ;;;***
 
-;;;### (autoloads nil "org-macs" "org-macs.el" (0 0 0 0))
+;;;### (autoloads nil "org-macs" "org-macs.el" "3517c9da87e8778e4abd7bb53586738e")
 ;;; Generated autoloads from org-macs.el
 
 (autoload 'org-load-noerror-mustsuffix "org-macs" "\
 Load FILE with optional arguments NOERROR and MUSTSUFFIX.
 
 \(fn FILE)" nil t)
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "org-macs" '("org-")))
 
 ;;;***
 
--- elpa/org-20201012/org-version.el
+++ elpa/org-20201012/org-version.el
@@ -11,7 +11,7 @@ Inserted by installing Org mode or when a release is made."
 (defun org-git-version ()
   "The Git version of Org mode.
 Inserted by installing Org or when a release is made."
-   (let ((org-git-version "9.4-17-g9a6689-elpa"))
+   (let ((org-git-version "9.4-19-gb1de0c-elpa"))
      org-git-version))
 
 (provide 'org-version)
