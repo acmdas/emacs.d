35a5a3d5b98805b095ff0099a1d95ac28ae6eaac
David <das@acm.org>
2020-12-17 19:09:28 -0500

    Added early-init.el, removed custom colors,added modus themes.

---
+++ elpa/modus-themes-20201215.1607/modus-themes-pkg.el
@@ -0,0 +1,12 @@
+(define-package "modus-themes" "20201215.1607" "Highly accessible themes (WCAG AAA)"
+  '((emacs "26.1"))
+  :commit "03070fea05a25c5f035c9d3ae8f1e3bdad05d07a" :keywords
+  ("faces" "theme" "accessibility")
+  :authors
+  (("Protesilaos Stavrou" . "info@protesilaos.com"))
+  :maintainer
+  ("Protesilaos Stavrou" . "info@protesilaos.com")
+  :url "https://gitlab.com/protesilaos/modus-themes")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
