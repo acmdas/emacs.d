ecd89966a424ef7bffdce3c6fe71e7bfc5f25e7a
David <das@acm.org>
2020-12-26 09:40:16 -0500

    Packages to upgrade: 11 (elfeed-20201205.134 markdown-mode-20201211.329 modus-themes-20201217.2115 org-journal-20201101.1438 dash-20201215.59 git-commit-20201115.2313 helm-20201215.1623 helm-core-20201202.907 magit-20201212.929 org-roam-20201216.1312 transient-20201205.1610). Packages to delete: 23 (with-editor use-package undo-tree transient smart-mode-line s rich-minority popup packed org-roam nov magit helm-descbinds helm-core helm git-commit f esxml emacsql-sqlite3 emacsql bind-key auto-compile async)

---
+++ elpa/org-journal-20201222.750/org-journal-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from org-journal.el  -*- no-byte-compile: t -*-
+(define-package "org-journal" "20201222.750" "a simple org-mode based journaling mode" '((emacs "25.1") (org "9.1")) :commit "c0836483ae43e525bf7547b7a789d171eda84c84" :authors '(("Bastian Bechtold") ("Christian Schwarzgruber")) :maintainer '("Bastian Bechtold") :url "http://github.com/bastibe/org-journal")
